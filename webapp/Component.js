bb9209e2ab10f95cffd485d338d184dd13019a22
Michelle Loh <michelleloh0414@gmail.com>
2024-07-03 14:10:46 +0000

    initial commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -3,33 +3,41 @@
  */
 
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "sap/btp/helloworldui5/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "sap/btp/helloworldui5/model/models",
+    "sap/base/Log"
+],
+function (UIComponent, Device, models, Log) {
+    "use strict";
 
-        return UIComponent.extend("sap.btp.helloworldui5.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend("sap.btp.helloworldui5.Component", {
+        metadata: {
+            manifest: "json"
+        },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+            //Add Logging
+            Log.info("Hello-World Information Log");
+            Log.debug("Hello-World Debug Log");
+            Log.warning("Hello-World Warning Log");
+            Log.error("Hello-World Error Log");
+            Log.fatal("Hello-World Fatal Log");
+            
+            // enable routing
+            this.getRouter().initialize();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        }
+    });
+}
 );
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
 ],
-function (Controller) {
+/** @param {typeof sap.ui.core.mvc.Controller} */
+function (Controller, MessageBox) {
     "use strict";
 
     return Controller.extend("sap.btp.helloworldui5.controller.View1", {
         onInit: function () {
-
+        },
+        onPress: function (){
+            MessageBox.alert("You have been alerted !");
         }
     });
 });
